bbfa001e8c1156ec1bd171e2fbda990cb6c745f4
VAmpersand <VP_ampersand_P@icloud.com>
2021-10-22 19:02:31 +0800

    Initial Commit

---
+++ MarsPhotos/MarsPhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarsPhotosApp.swift
+//  MarsPhotos
+//
+//  Created by Viktor Prokolota on 22.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MarsPhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
