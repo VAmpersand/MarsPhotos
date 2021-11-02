c1e1aa09b6b440929aa2b04a8444c2d0f962f739
VAmpersand <VP_ampersand_P@icloud.com>
2021-11-02 15:17:33 +0800

    Added mission info

--- MarsPhotos/Helpers/Constants.swift
+++ MarsPhotos/Helpers/Constants.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 import PureSwiftUI
 
-
 struct Constants {
     static let layoutConfig = LayoutGuideConfig.grid(columns: 100, rows: 126)
     
     static let apiKey = "eb0Q4TEg6PKMZTlKogsUBgxQWGHH1SrWEgqfQxP4"
+    
+    static func getOficialSiteURL(for rover: Rover) -> URL {
+        var urlStr: String {
+            switch rover {
+            case .opportunity: return "https://www.jpl.nasa.gov/missions/mars-exploration-rover-opportunity-mer"
+            case .spirit: return "https://www.jpl.nasa.gov/missions/mars-exploration-rover-spirit-mer-spirit"
+            case .curiosity: return "https://mars.nasa.gov/msl/mission/overview/"
+            }
+        }
+        
+        guard let url = URL(string: urlStr) else  {
+            fatalError("Error: Oficial site URL faled")
+        }
+        
+        return url
+    }
+    
+    static let offset = UIScreen.screenWidth * 0.04
 }
--- MarsPhotos/RoverSelectionView/RoverSelectionView.swift
+++ MarsPhotos/RoverSelectionView/RoverSelectionView.swift
@@ -7,80 +7,130 @@
 
 import SwiftUI
 import PureSwiftUI
+import SafariServices
 
+
+// MARK: - RoverSelectionView
 struct RoverSelectionView: View {
     
     @ObservedObject var viewModel = RoverSelectionViewModel()
     
     @State private var selectedRover: Rover = .opportunity
+    @State var showSafari = false
     
     var body: some View {
-        
-        VStack {
-            ZStack {
-                ForEach(Rover.allCases, id: \.self) { rover in
-                    Image(rover.rawValue)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(UIScreen.screenWidth, UIScreen.screenWidth * 1.255, .trailing)
-                        .modifier(ClipRoverShape(rover: rover, selectedRover: selectedRover))
-                        .onTapGesture {
-                            withAnimation {
-                                selectedRover = rover
-                            }
+        ZStack {
+            ScrollView(showsIndicators: false) {
+                VStack {
+                    ZStack {
+                        ForEach(Rover.allCases, id: \.self) { rover in
+                            Image(rover.rawValue)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(UIScreen.screenWidth, UIScreen.screenWidth * 1.255, .trailing)
+                                .modifier(ClipAnimatedShape(rover: rover, selectedRover: selectedRover))
+                                .onTapGesture {
+                                    withAnimation { selectedRover = rover }
+                                }
                         }
+                    }.padding(.top, 10)
+                    
+                    Text(Strings.roverTitle.uppercased())
+                        .padding(.horizontal, Constants.offset)
+                        .padding(.top, 10)
+                        .font(Font.custom(Fonts.latoHeavy.rawValue, fixedSize: 12))
+                        .frame(UIScreen.screenWidth, .infinity, .leading)
+                        .foregroundColor(Color.init(hex: Colors.titleGray.rawValue))
+                    
+                    TabView(selection: $selectedRover.animation(.linear(duration: 0.15))) {
+                        ForEach(Rover.allCases, id: \.self) { rover in
+                            getTitleView(for: rover)
+                        }
+                    }
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                    .frame(UIScreen.screenWidth, 250, .leading)
+                    
+                    Spacer()
+                        .frame(height: 100)
                 }
             }
             
-            Text("FETCH PHOTO FROM ROVER")
-                .padding(.top)
-                .padding(.leading)
-                .font(Font.custom(Fonts.latoHeavy.rawValue, fixedSize: 12))
-                .frame(UIScreen.screenWidth, .infinity, .leading)
-                .foregroundColor(Color.init(hex: Colors.titleGray.rawValue))
-            
-            TabView(selection: $selectedRover) {
-                ForEach(Rover.allCases, id: \.self) { rover in
-                    getTitleViewFor(rover: rover)
-                }
-            }
-            .disabled(true)
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-            .frame(UIScreen.screenWidth, 40, .leading)
-            
-            
-            Spacer()
-            
-            Button {
-                self.viewModel.currentRover = self.selectedRover
+            VStack {
+                Spacer()
                 
-            } label: {
-                Text("Fetch")
-                    .font(Font.custom(Fonts.latoHeavy.rawValue, fixedSize: 15))
-                    .frame(UIScreen.screenWidth - 100, .infinity)
-                    .foregroundColor(Color.white)
-                    .padding()
+                Button {
+                    self.viewModel.currentRover = self.selectedRover
+                    
+                } label: {
+                    Text("Fetch")
+                        .font(Font.custom(Fonts.latoHeavy.rawValue, fixedSize: 15))
+                        .frame(UIScreen.screenWidth - 100, .infinity)
+                        .foregroundColor(Color.white)
+                        .padding()
+                }
+                .background(Color.blue)
+                .frame(.infinity, 50, .bottom)
+                .clipCapsule()
+                .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: -3)
+                .padding(.horizontal, 50)
+                .padding(.bottom, 30)
             }
-            .background(Color.blue)
-            .frame(UIScreen.screenWidth - 100, 50)
-            .clipCapsule()
-            .padding()
         }
         .ignoresSafeArea()
     }
     
     @ViewBuilder
-    func getTitleViewFor(rover name: Rover) -> some View {
-        Text(name.rawValue.firstUppercased)
-            .offset(UIScreen.screenWidth * 0.04, 0)
-            .font(Font.custom(Fonts.abrilFatface.rawValue, fixedSize: 32))
-            .frame(UIScreen.screenWidth, .infinity, .leading)
-            .foregroundColor(Color.init(hex: Colors.titleDarkGray.rawValue))
+    func getTitleView(for rover: Rover) -> some View {
+        VStack {
+            Text(rover.rawValue.firstUppercased)
+                .font(Font.custom(Fonts.abrilFatface.rawValue, fixedSize: 32))
+                .padding(.horizontal, Constants.offset)
+                .frame(UIScreen.screenWidth, .infinity, .leading)
+                .foregroundColor(Color.init(hex: Colors.titleDarkGray.rawValue))
+            
+            Text(Strings.missionTitle.uppercased())
+                .padding(.horizontal, Constants.offset)
+                .padding(.top, 10)
+                .font(Font.custom(Fonts.latoHeavy.rawValue, fixedSize: 12))
+                .frame(UIScreen.screenWidth, .infinity, .leading)
+                .foregroundColor(Color.init(hex: Colors.titleGray.rawValue))
+              
+            Text(Strings.getMissionInfo(for: rover))
+                .padding(.horizontal, Constants.offset)
+                .padding(.top, 3)
+                .font(Font.custom(Fonts.latoHeavy.rawValue, fixedSize: 12))
+                .frame(UIScreen.screenWidth, .infinity, .leading)
+                .foregroundColor(Color.init(hex: Colors.titleGray.rawValue).opacity(0.9))
+                .lineLimit(10)
+            
+            HStack {
+                Text(Strings.moreLabel.uppercased())
+                    .font(Font.custom(Fonts.latoHeavy.rawValue, fixedSize: 12))
+                    .frame(.infinity, .infinity, .trailing)
+                    .foregroundColor(Color.init(hex: Colors.titleGray.rawValue))
+                
+                Image(systemName: "ellipsis.circle")
+                    .renderingMode(.template)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(20)
+                    .foregroundColor(Color(hex: Colors.titleGray.rawValue))
+                
+            }
+            .padding(.horizontal, Constants.offset)
+            .frame(UIScreen.screenWidth, 32, .trailing)
+        }
+        .onTapGesture {
+            self.showSafari = true
+        }
+        .sheet(isPresented: $showSafari) {
+            SafariView(url: Constants.getOficialSiteURL(for: rover))
+        }
     }
 }
 
-
-struct ClipRoverShape: ViewModifier {
+// MARK: - ClipAnimatedShape
+struct ClipAnimatedShape: ViewModifier {
     
     var rover: Rover
     var selectedRover: Rover
@@ -94,6 +144,7 @@ struct ClipRoverShape: ViewModifier {
                 .clipShape(OpportunitySegment())
                 .contentShape(OpportunitySegment())
                 .scaleEffect(rover == selectedRover ? 1.2 : 1)
+                .animation(.linear(duration: 0.15))
                 .offset(-offset * 0.2, -offset * 1.3)
             
         case .spirit:
@@ -101,6 +152,7 @@ struct ClipRoverShape: ViewModifier {
                 .clipShape(SpiritSegment())
                 .contentShape(SpiritSegment())
                 .scaleEffect(rover == selectedRover ? 1.2 : 1)
+                .animation(.linear(duration: 0.15))
                 .offset(-offset * 4, -offset * 5)
             
         case .curiosity:
@@ -108,13 +160,30 @@ struct ClipRoverShape: ViewModifier {
                 .clipShape(CuriositySegment())
                 .contentShape(CuriositySegment())
                 .scaleEffect(rover == selectedRover ? 1.2 : 1)
+                .animation(.linear(duration: 0.15))
                 .offset(offset * 0.07, -offset * 1.2)
         }
     }
 }
 
-//struct RoverSelectionView_Previewer: PreviewProvider {
-//    static var previews: some View {
-//        RoverSelectionView()
-//    }
-//}
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+        
+    }
+    
+}
+
+// MARK: - RoverSelectionView_Previewer
+struct RoverSelectionView_Previewer: PreviewProvider {
+    static var previews: some View {
+        RoverSelectionView()
+    }
+}
